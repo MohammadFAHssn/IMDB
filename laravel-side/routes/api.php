c0bb2fe8cdacbc8844c5e16bff023080752a0432
MohammadFHssn <mohammad.hassani.aa@gmail.com>
2025-02-13 22:56:17 +0330

    movie/getAll api

--- laravel-side/routes/api.php
+++ laravel-side/routes/api.php
@@ -6,3 +6,7 @@ use Illuminate\Support\Facades\Route;
 Route::middleware(['auth:sanctum'])->get('/user', function (Request $request) {
     return $request->user();
 });
+
+Route::controller(MovieController::class)->group(function () {
+    Route::get('movie/getAll', 'getAll');
+});

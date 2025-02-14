d06bec50fc2744887489a53ca45b30dcac1fec06
MohammadFHssn <mohammad.hassani.aa@gmail.com>
2025-02-14 19:16:27 +0330

    getAll from repository

--- laravel-side/app/Services/MovieService.php
+++ laravel-side/app/Services/MovieService.php
@@ -2,10 +2,19 @@
 
 namespace App\Services;
 
+use App\Repositories\MovieRepository;
+
 class MovieService
 {
+    protected $movieRepository;
+
+    public function __construct()
+    {
+        $this->movieRepository = new MovieRepository();
+    }
+
     public function getAll()
     {
-        return ['result' => 'success'];
+        return $this->movieRepository->getAll();
     }
 }

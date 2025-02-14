6e7de5b146c11eabd2b5ed6e1037394fd26eecb4
MohammadFHssn <mohammad.hassani.aa@gmail.com>
2025-02-14 18:50:07 +0330

    set genre_movie migration and model

--- laravel-side/app/Models/GenreMovie.php
+++ laravel-side/app/Models/GenreMovie.php
@@ -6,5 +6,6 @@ use Illuminate\Database\Eloquent\Model;
 
 class GenreMovie extends Model
 {
-    //
+    protected $table = 'genre_movie';
+
 }
--- laravel-side/database/migrations/2025_02_14_142923_create_genre_movie_table.php
+++ laravel-side/database/migrations/2025_02_14_142923_create_genre_movie_table.php
@@ -4,15 +4,19 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-return new class extends Migration
-{
+return new class extends Migration {
     /**
      * Run the migrations.
      */
     public function up(): void
     {
-        Schema::create('genre_movies', function (Blueprint $table) {
+        Schema::create('genre_movie', function (Blueprint $table) {
             $table->id();
+
+            $table->foreignId('movie_id')->constrained()->onDelete('cascade');
+            $table->foreignId('genre_id')->constrained()->onDelete('cascade');
+
+
             $table->timestamps();
         });
     }
@@ -22,6 +26,6 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('genre_movies');
+        Schema::dropIfExists('genre_movie');
     }
 };

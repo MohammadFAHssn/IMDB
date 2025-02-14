71cbfecb9903777970341ba7f0910de7cbbc88de
MohammadFHssn <mohammad.hassani.aa@gmail.com>
2025-02-14 17:31:45 +0330

    set movies migration

--- laravel-side/database/migrations/2025_02_14_080144_create_movies_table.php
+++ laravel-side/database/migrations/2025_02_14_080144_create_movies_table.php
@@ -4,8 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-return new class extends Migration
-{
+return new class extends Migration {
     /**
      * Run the migrations.
      */
@@ -13,6 +12,16 @@ return new class extends Migration
     {
         Schema::create('movies', function (Blueprint $table) {
             $table->id();
+
+            $table->tinyText('IMDB_id');
+            $table->tinyText('title');
+            $table->tinyText('type');
+            $table->text('primary_image_url')->nullable();
+            $table->unsignedSmallInteger('release_year');
+            $table->unsignedSmallInteger('end_year')->nullable();
+            $table->double('aggregate_rating');
+            $table->unsignedInteger('vote_count');
+
             $table->timestamps();
         });
     }

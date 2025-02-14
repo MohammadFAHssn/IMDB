85f84034d1e3f19b9d6fbbbea8606c90a619dd5a
MohammadFHssn <mohammad.hassani.aa@gmail.com>
2025-02-14 11:04:18 +0330

    define axios globally and set baseURL and  headers

--- vue-side/src/main.js
+++ vue-side/src/main.js
@@ -6,7 +6,16 @@ import { createPinia } from 'pinia'
 import App from './App.vue'
 import router from './router'
 
+import axios from 'axios'
+
 const app = createApp(App)
+
+app.config.globalProperties.$axios = axios.create({
+  baseURL: 'http://127.0.0.1:8000/api',
+  headers: {
+    'Content-Type': 'application/json',
+  },
+})
 
 app.use(createPinia())
 app.use(router)

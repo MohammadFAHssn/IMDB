3069304f2abc9334a20642050f9df2a45d51e21a
MohammadFHssn <mohammad.hassani.aa@gmail.com>
2025-02-16 19:16:16 +0330

    show movies

--- vue-side/src/components/Movie.vue
+++ vue-side/src/components/Movie.vue
@@ -1,20 +1,97 @@
 <template>
-  <div>!</div>
+  <section class="movies-sec">
+    <button class="sync-btn">Sync with IMDB</button>
+
+    <section class="ag-grid-sec">
+      <ag-grid-vue style="width: 100%; height: 100%" :rowData="rowData" :columnDefs="colDefs">
+      </ag-grid-vue>
+    </section>
+  </section>
 </template>
 
 <script>
 export default {
+  data() {
+    return {
+      movies: [],
+
+      rowData: [],
+      colDefs: [],
+    }
+  },
+
   methods: {
     getAllMovies() {
       this.$axios
         .get('movie/getAll')
-        .then(() => {})
+        .then(({ data }) => {
+          this.movies = data
+          this.setRowData()
+        })
         .catch(() => {})
+    },
+
+    setRowData() {
+      this.rowData = this.movies.map((movie) => {
+        return {
+          IMDB_id: movie.IMDB_id,
+          title: movie.title,
+          type: movie.type,
+          primaryImageUrl: movie.primary_image_url,
+          releaseYear: movie.release_year,
+          endYear: movie.end_year,
+          aggregateRating: movie.aggregate_rating,
+          voteCount: movie.vote_count,
+        }
+      })
+    },
+
+    setColDefs() {
+      this.colDefs = [
+        { field: 'IMDB_id' },
+        { field: 'title' },
+        { field: 'type' },
+        { field: 'primaryImageUrl' },
+        { field: 'releaseYear' },
+        { field: 'endYear' },
+        { field: 'aggregateRating' },
+        { field: 'voteCount' },
+      ]
     },
   },
 
   created() {
+    this.setColDefs()
+
     this.getAllMovies()
   },
 }
 </script>
+
+<style scoped>
+.movies-sec {
+  box-sizing: border-box;
+
+  height: 100vh;
+
+  padding: 1em;
+
+  display: grid;
+  grid-template-columns: auto;
+  grid-template-rows: auto 1fr;
+
+  row-gap: 1em;
+}
+
+.ag-grid-sec {
+  height: 100%;
+}
+
+.sync-btn {
+  padding: 0.5em;
+
+  justify-self: start;
+
+  cursor: pointer;
+}
+</style>

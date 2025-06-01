d0a0048113922fefa4960794300081cdaa425fec
Mohammad <Mohammad.Hassani.AA@gmail.com>
2025-06-01 19:03:38 +0330

    migrate to vue 3

--- vue-side/src/components/Movie.vue
+++ vue-side/src/components/Movie.vue
@@ -16,172 +16,183 @@
   </section>
 </template>
 
-<script>
+<script setup>
 import axios from 'axios'
+import { ref, onMounted, watch } from 'vue'
 
-export default {
-  data() {
-    return {
-      movies: [],
+const movies = ref([])
 
-      rowData: [],
-      colDefs: [],
+const rowData = ref([])
+const colDefs = ref([])
 
-      includeRatedMovies: false,
-      ratedMovies: [],
-    }
-  },
+const includeRatedMovies = ref(false)
+const ratedMovies = ref([])
 
-  methods: {
-    getAllMovies() {
-      axios
-        .get('http://127.0.0.1:8000/api/movie/getAll')
-        .then(({ data }) => {
-          this.movies = data
-          this.setRowData()
-        })
-        .catch(() => {})
-    },
+const IMDB_movies = ref([])
 
-    setRowData() {
-      this.rowData = this.movies
-        .filter((movie) => this.includeRatedMovies || !this.ratedMovies.includes(movie.IMDB_id))
-        .map((movie) => {
-          return {
-            IMDB_id: movie.IMDB_id,
-            title: movie.title,
-            type: movie.type,
-            primaryImageUrl: movie.primary_image_url,
-            releaseYear: movie.release_year,
-            endYear: movie.end_year,
-            aggregateRating: movie.aggregate_rating,
-            voteCount: movie.vote_count,
-            myRating: Math.round(movie.vote_count * movie.aggregate_rating),
-          }
-        })
-    },
+const getAllMovies = () => {
+  axios
+    .get('http://127.0.0.1:8000/api/movie/getAll')
+    .then(({ data }) => {
+      movies.value = data
+      setRowData()
+    })
+    .catch(() => {})
+}
 
-    setColDefs() {
-      this.colDefs = [
-        { field: 'IMDB_id' },
-        { field: 'title' },
-        { field: 'type' },
-        { field: 'primaryImageUrl' },
-        { field: 'releaseYear' },
-        { field: 'endYear' },
-        { field: 'aggregateRating' },
-        { field: 'voteCount' },
-        { field: 'myRating' },
-      ]
-    },
+const setRowData = () => {
+  let sumOfAggregateRating = 0
+  let sumOfVoteCount = 0
+  movies.value.forEach((movie) => {
+    sumOfAggregateRating = sumOfAggregateRating + movie.aggregate_rating
+    sumOfVoteCount = sumOfVoteCount + movie.vote_count
+  })
 
-    onSyncClick() {
-      axios
-        .post(
-          'https://caching.graphql.imdb.com/',
-          {
-            operationName: 'AdvancedTitleSearch',
-            variables: {
-              explicitContentConstraint: { explicitContentFilter: 'INCLUDE_ADULT' },
-              first: 1000,
-              locale: 'en-US',
-              sortBy: 'USER_RATING_COUNT',
-              sortOrder: 'DESC',
-              userRatingsConstraint: { aggregateRatingRange: { max: 10, min: 6.9 } },
-            },
-            extensions: {
-              persistedQuery: {
-                sha256Hash: '6842af47c3f1c43431ae23d394f3aa05ab840146b146a2666d4aa0dc346dc482',
-                version: 1,
-              },
-            },
-          },
-          {
-            headers: { 'Content-Type': 'application/json; charset=utf-8' },
-          },
-        )
-        .then(({ data }) => {
-          let IMDB_movies = data.data.advancedTitleSearch.edges.map((edge) => {
-            return {
-              IMDB_id: edge.node.title.id,
-              title: edge.node.title.titleText.text,
-              type: edge.node.title.titleType.text,
-              primary_image_url: edge.node.title.primaryImage.url,
-              release_year: edge.node.title.releaseYear.year,
-              end_year: edge.node.title.releaseYear.endYear,
-              aggregate_rating: edge.node.title.ratingsSummary.aggregateRating,
-              vote_count: edge.node.title.ratingsSummary.voteCount,
-            }
-          })
+  const average_aggregateRating = sumOfAggregateRating / movies.value.length
+  const average_voteCount = sumOfVoteCount / movies.value.length
 
-          this.syncWithIMDB(IMDB_movies)
-        })
-        .catch((error) => {
-          console.error('Error:', error)
-        })
-    },
+  console.log(average_aggregateRating)
+  console.log(average_voteCount)
 
-    syncWithIMDB(IMDB_movies) {
-      axios
-        .post('http://127.0.0.1:8000/api/movie/syncWithIMDB', {
-          IMDB_movies: IMDB_movies,
-        })
-        .then(() => {
-          this.getAllMovies()
-        })
-        .catch(() => {})
-    },
+  rowData.value = movies.value
+    .filter((movie) => includeRatedMovies.value || !ratedMovies.value.includes(movie.IMDB_id))
+    .map((movie) => {
+      return {
+        IMDB_id: movie.IMDB_id,
+        title: movie.title,
+        type: movie.type,
+        primaryImageUrl: movie.primary_image_url,
+        releaseYear: movie.release_year,
+        endYear: movie.end_year,
+        aggregateRating: movie.aggregate_rating,
+        voteCount: movie.vote_count,
+        myRating:
+          (movie.vote_count * movie.aggregate_rating +
+            average_voteCount * average_aggregateRating) /
+          (movie.vote_count + average_voteCount),
+      }
+    })
+}
 
-    getRatedMovies() {
-      const url = 'https://api.graphql.imdb.com/'
+const setColDefs = () => {
+  colDefs.value = [
+    { field: 'IMDB_id' },
+    { field: 'title' },
+    { field: 'type' },
+    { field: 'primaryImageUrl' },
+    { field: 'releaseYear' },
+    { field: 'endYear' },
+    { field: 'aggregateRating' },
+    { field: 'voteCount' },
+    { field: 'myRating' },
+  ]
+}
 
-      const payload = {
-        operationName: 'RatingsPage',
+const onSyncClick = async () => {
+  await Promise.all([getMoviesFromIMDB(), getRatedMovies()])
+  syncWithIMDB()
+}
+
+const getMoviesFromIMDB = () => {
+  return axios
+    .post(
+      'https://caching.graphql.imdb.com/',
+      {
+        operationName: 'AdvancedTitleSearch',
         variables: {
-          filter: {
-            explicitContentConstraint: { explicitContentFilter: 'INCLUDE_ADULT' },
-            singleUserRatingConstraint: { filterType: 'INCLUDE', userId: 'ur115987309' },
-          },
-          first: 250,
+          explicitContentConstraint: { explicitContentFilter: 'INCLUDE_ADULT' },
+          first: 1000,
           locale: 'en-US',
-          sort: { sortBy: 'SINGLE_USER_RATING', sortOrder: 'ASC' },
+          sortBy: 'USER_RATING_COUNT',
+          sortOrder: 'DESC',
+          userRatingsConstraint: { aggregateRatingRange: { max: 10, min: 6.9 } },
         },
         extensions: {
           persistedQuery: {
-            sha256Hash: '90e077634ebba07351579909fe00a18052873cdd278133cb5f0bc2a6efd25a6d',
+            sha256Hash: '6842af47c3f1c43431ae23d394f3aa05ab840146b146a2666d4aa0dc346dc482',
             version: 1,
           },
         },
-      }
-
-      axios
-        .post(url, payload, {
-          headers: {
-            'Content-Type': 'application/json',
-          },
-        })
-        .then((response) => {
-          this.ratedMovies = response.data.data.advancedTitleSearch.edges.map(
-            (edge) => edge.node.title.id,
-          )
-
-          this.getAllMovies()
-        })
-        .catch(() => {})
-    },
-  },
-
-  created() {
-    this.setColDefs()
-    this.getRatedMovies()
-  },
-
-  watch: {
-    includeRatedMovies() {
-      this.setRowData()
-    },
-  },
+      },
+      {
+        headers: { 'Content-Type': 'application/json; charset=utf-8' },
+      },
+    )
+    .then(({ data }) => {
+      IMDB_movies.value = data.data.advancedTitleSearch.edges.map((edge) => {
+        return {
+          IMDB_id: edge.node.title.id,
+          title: edge.node.title.titleText.text,
+          type: edge.node.title.titleType.text,
+          primary_image_url: edge.node.title.primaryImage.url,
+          release_year: edge.node.title.releaseYear.year,
+          end_year: edge.node.title.releaseYear.endYear,
+          aggregate_rating: edge.node.title.ratingsSummary.aggregateRating,
+          vote_count: edge.node.title.ratingsSummary.voteCount,
+        }
+      })
+    })
+    .catch((error) => {
+      console.error('Error:', error)
+    })
 }
+
+const syncWithIMDB = () => {
+  axios
+    .post('http://127.0.0.1:8000/api/movie/syncWithIMDB', {
+      IMDB_movies: IMDB_movies.value,
+      ratedMovies: ratedMovies.value,
+    })
+    .then(() => {
+      getAllMovies()
+    })
+    .catch(() => {})
+}
+
+const getRatedMovies = () => {
+  const url = 'https://api.graphql.imdb.com/'
+
+  const payload = {
+    operationName: 'RatingsPage',
+    variables: {
+      filter: {
+        explicitContentConstraint: { explicitContentFilter: 'INCLUDE_ADULT' },
+        singleUserRatingConstraint: { filterType: 'INCLUDE', userId: 'ur115987309' },
+      },
+      first: 250,
+      locale: 'en-US',
+      sort: { sortBy: 'SINGLE_USER_RATING', sortOrder: 'ASC' },
+    },
+    extensions: {
+      persistedQuery: {
+        sha256Hash: '90e077634ebba07351579909fe00a18052873cdd278133cb5f0bc2a6efd25a6d',
+        version: 1,
+      },
+    },
+  }
+
+  return axios
+    .post(url, payload, {
+      headers: {
+        'Content-Type': 'application/json',
+      },
+    })
+    .then((response) => {
+      ratedMovies.value = response.data.data.advancedTitleSearch.edges.map(
+        (edge) => edge.node.title.id,
+      )
+    })
+    .catch(() => {})
+}
+
+onMounted(() => {
+  getAllMovies()
+  setColDefs()
+})
+
+watch(includeRatedMovies, () => {
+  setRowData()
+})
 </script>
 
 <style scoped>
